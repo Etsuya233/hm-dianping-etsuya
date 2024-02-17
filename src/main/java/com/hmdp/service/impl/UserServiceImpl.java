8d7a8ada85c30b8afbac5506c4b0a0bfc2b64279
Etsuya233 <etsuya233@outlook.com>
2024-02-17 17:22:12 +0800

    add: session发送验证码

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -15,6 +22,23 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+	@Override
+	public Result sendCode(String phone, HttpSession session) {
+		//校验手机号
+		if(RegexUtils.isPhoneInvalid(phone)){
+			return Result.fail("手机号格式错误！");
+		}
+		//生成6位验证码
+		String code = RandomUtil.randomNumbers(6);
+		//将验证码传入Session
+		session.setAttribute("code", code);
+		log.info("验证码发送成功：{}", code);
+		//TODO 真的发送验证码
+		return Result.ok();
+	}
 
 }
